5dedd846ddad9857de506c8085bc713694e8483a
ZijianYang <yang20040764@163.com>
2018-01-04 14:13:09 +0800

    a

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,11 +5,19 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    // return Promise.resolve(HEROES);
-    return new Promise(resolve => {
-        // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(this.getHeroes()), 2000);
-      });
-  }
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+
+    };
+    getSetTimeHeroes(): Promise<Hero[]> {
+        // return Promise.resolve(HEROES);
+        return new Promise(resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
+    };
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component,OnInit  } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -17,7 +19,9 @@ export class HeroesComponent  implements OnInit  {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router,) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -30,4 +34,8 @@ export class HeroesComponent  implements OnInit  {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
